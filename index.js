aa580138f599c9dcc6654bbe63e8e51adf267b58
hauntedbae <rima.sakalauskaite4121@gmail.com>
2022-07-27 22:00:23 +0300

    Add node.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const casual = require("casual");
+
+console.log("Rima Sakalauskaite");
+console.log(casual.city);
+console.log(casual.city, casual.integer(1, 10));
+console.log(casual.suffix, casual.first_name, casual.last_name);
